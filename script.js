e4a50f979f7202c3a627d980f5aea746e9c57ac2
aecarena <116913112+aecarena@users.noreply.github.com>
2022-12-04 13:56:49 -0800

    Add files via upload
    updated with extensive add ons and changes

--- script.js
+++ script.js
@@ -1,71 +1,40 @@
-var characterLength= 8;
-var choiceArray = [];
-
-var specialCharacterArray= ['!','@','#','$','%','^','&','*','(',')','_','-','=','+','<','>','?','.',',',':',';','~'];
-var lowerCaseArray= ['a','b','c','d','e','f','g','h','i','j','k','l','m','o','p','q','r','s','t','u','v','w','x','y','z'];
-var upperCaseArray = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']; 
-var numberArray = ['0','1','2','3','4','5','6','7','8','9'];
-
-// Assignment Code
-var generateButton=document.querySelector("#generate");
+//create timer that counts down
+var timeEl = document.querySelector(".timer")
 
 
+//TIME SET TO COUNT DOWN
+var secondsLeft = 10;
 
-// Add event listener to generate button
-generateButton.addEventListener ("click", writePassword);
+function setTime() {
+  // Sets interval in variable
+  var timerInterval = setInterval(function() {
+    secondsLeft--;
+    timeEl.textContent = secondsLeft + " seconds remaining";
 
+    if(secondsLeft === 0) {
+      // Stops execution of action at set interval
+      clearInterval(timerInterval);
+      // Calls function to create a message
+      sendMessage();
+    }
 
-//determine length
-function getprompt() {
-  choiceArray = [];
+  }, 1000);
+}
 
-  characterLength = parseInt(prompt("What is your desired length of password? (Between 8 and 128 characters)"))
+// Function to create message
+function sendMessage() {
+    timeEl.textContent = "TIMES UP!";
   
-  if (isNaN(characterLength)||(characterLength < 8) || (characterLength > 128)){
-    alert ("Desired length must be a number within 8 to 128. Please input your new answer.")
-    return false;
   }
-  if (confirm ("would you like special characters in your password")){
-    choicesArray = choiceArray.concat(specialCharacterArray);
-  }
-  if (confirm ("would you like Lower case characters in your password")){
-    choicesArray = choiceArray.concat(lowerCaseArray);
-  }
-  if (confirm ("would you like Upper case characters in your password")){
-    choicesArray = choiceArray.concat(upperCaseArray);
-  }
-  if (confirm ("would you like number characters in your password")){
-    choicesArray = choiceArray.concat(numberArray);
-  }
-  return true;
-}
-console.log (getprompt())
-
-function generatePassword() {
-  //generate password based on answers from prompts
-  var password = "";
-  for (var i = 0; i < characterLength; i++) {
-    var randomIndex = math.floor(Math.random() * choiceArray.length)
-    password = password + choiceArray[randomIndex];
-  }
-  return password;
-
-}
-
   
-// Write password to the #password input
-function writePassword() {
-  var correctPrompts = getprompts();
-  var passwordText = document.querySelector("#password");
+  setTime();
 
-  if (correctPrompts){
-    var newpassword = generatePassword();
-    passwordText.value = newpassword;
-  } else {
-    passwordText.value = "";
+//function generateQuiz()
+
+
+function hidden (elements) {
+  elements = elements.length ? elements : [elements];
+  for (var index = 0; index < elements.length; index++) {
+    elements[index].style.display = 'none';
   }
- 
 }
-
-console.log (writePassword)
-
