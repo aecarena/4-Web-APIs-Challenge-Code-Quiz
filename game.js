4a2d0e969a3cf8228e05ca2717ae0ff03aa6c4a9
aecarena <116913112+aecarena@users.noreply.github.com>
2022-12-04 19:30:52 -0800

    add file via upload
    worked on new structure and questions

---
+++ game.js
@@ -0,0 +1,69 @@
+
+let questions = [
+    {
+        numb: 1,
+        question: "What does HTML stand for?",
+        answer: "Hyper Text Markup Language",
+        options: [
+            "Cascading Style Sheets",
+            "Hyper Text Markup Language",
+            "Application Programing Language",
+            
+        ]
+    },
+    {
+        numb: 2,
+        question: "What does CSS stand for?",
+        answer: "Cascading Style Sheets",
+        options: [
+            "Cascading Style Sheets",
+            "Hyper Text Markup Language",
+            "Application Programing Language",
+            
+        ]
+    },
+    {
+        numb: 3,
+        question: "What does API stand for?",
+        answer: "Application Programing Interface",
+        options: [
+            "Cascading Style Sheets",
+            "Hyper Text Markup Language",
+            "Application Programing Interface",
+           
+        ]
+    },
+    {
+        numb: 4,
+        question: "What are the four types of API?",
+        answer: "public, partner, private and composite",
+        options: [
+            "public, partner, private and composite",
+            "JAVA, JS,CSS, HTML",
+            "none of the above",
+           
+        ]
+    },
+    {
+        numb: 5,
+        question: "What is an example of Geolocation API?",
+        answer: "Google maps API",
+        options: [
+            "peanut butter and jelly",
+            "Google maps API",
+            "Twitter API",
+           
+        ]
+    },
+    {
+        numb: 6,
+        question: "What does DOM stand for",
+        answer: "Document Object Model",
+        options: [
+            "driver of machine",
+            "document objective mode",
+            "Document Object Model",
+           
+        ]
+    },
+];
